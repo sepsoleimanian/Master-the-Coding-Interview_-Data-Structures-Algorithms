a488f91c7985e27e1300839c2964ae71ee8ab8af
Sepideh Soleimanian <sepsoleimanian@gmail.com>
2024-09-23 12:43:07 +0330

    added timer

--- Big_O/1.py
+++ Big_O/1.py
@@ -1,10 +1,19 @@
+import time 
 my_list = ["Nemo"]
 
 def findNemo(array):
+    start_time = time.perf_counter()  # Start time inside the function
+
     for i in range(len(array)): 
         if array[i] == "Nemo":
             print("Found Nemo! :)")
-            return 
-    print("Nemo Not Found. :()") 
+            end_time = time.perf_counter()  # End time after finding Nemo
+            execution_time = end_time - start_time
+            print(f"Execution time: {execution_time} seconds")
+            return
+    print("Nemo Not Found. :(")  
+    end_time = time.perf_counter()  
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time} seconds")
 
-findNemo(my_list)  
+findNemo(my_list)
