948d4c502cda7be6251d701ec4da2e38b0722969
Sepideh Soleimanian <sepsoleimanian@gmail.com>
2024-10-07 17:12:53 +0330

    Second code of the course

---
+++ Big_O/Big_O_and_scalability.py
@@ -0,0 +1,15 @@
+import time
+
+nemo = ["nemo"]
+names = ["sep"] * 1000000
+
+def find_nemo(array):
+    # t0 = time.perf_counter()
+    for i in range(len(array)):
+        if array[i] == "nemo":
+            print("Found Nemo!")
+    # elapsed_time = time.perf_counter() - t0
+    print(f"Elapsed time: {elapsed_time} seconds")
+
+find_nemo(nemo)
+find_nemo(names)
