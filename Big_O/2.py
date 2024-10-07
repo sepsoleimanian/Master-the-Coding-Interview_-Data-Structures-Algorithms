a529311fe0166a3b43118a08f96d1e6bedbc34bc
Sepideh Soleimanian <sepsoleimanian@gmail.com>
2024-10-07 15:39:30 +0330

    First code of the course

--- Big_O/2.py
+++
@@ -1,7 +0,0 @@
-import time 
-my_list = [0,1,2,3,4,5,6,7,8,9]
-
-def O1(array):
-      print(my_list[1])  
-
-O1(my_list) # O(1) --> Constant Time
---
+++ Big_O/What_is_good_code.py
@@ -0,0 +1,7 @@
+nemo = ["nemo"]
+def find_nemo(array):
+  for i in range(len(array)):
+    if array[i] == "nemo":
+      print("Found_Nemo!")
+
+find_nemo(nemo)
